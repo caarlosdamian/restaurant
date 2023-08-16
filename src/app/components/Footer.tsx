d63b967cb0a146cc72d6fb7d80523c6b09b861f5
caarlosdamian <dapecarlos@gmail.com>
2023-08-15 21:32:42 -0600

    feat(app): finished front end app

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="h-12 md:h-24 p-4 lg:p-20 xl:0:40 text-red-500 flex items-center justify-between">
+    <footer className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
       <Link href="/" className="font-bold text-lg">
         MASSIMO
       </Link>
