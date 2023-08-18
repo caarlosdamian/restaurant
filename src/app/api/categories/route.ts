a063486633c8a9b7dca910fd9a9ea05eceb3c2e7
Carlos Damian <carlos.damian@choicehotels.com>
2023-08-18 17:07:58 -0600

    feat(api): added prisma and category endpoint

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(
+        JSON.stringify(categories),
+        { status: 200 }
+      );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went worng!' }),
+      { status: 500 }
+    );
+  }
+};
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,11 +1,23 @@
-import { menu } from '@/data';
+import { Menutype } from '@/types/types';
 import Link from 'next/link';
 import React from 'react';
 
-const MenuPage = () => {
+const getData = async () => {
+  const res = await fetch('http://localhost:3000/api/categories', {
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    throw new Error('Failed');
+  }
+
+  return res.json();
+};
+
+const MenuPage = async () => {
+  const menu: Menutype = await getData();
   return (
     <div className="p-4 lg:px-20 xl:p-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
-      {menu.map((category) => (
+      {menu?.map((category) => (
         <Link
           href={`menu/${category.slug}`}
           key={category.id}
