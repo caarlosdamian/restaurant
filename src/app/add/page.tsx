8c5cbb357b3ade06b942937cc89bd131c586f441
Carlos Damian <carlos.damian@choicehotels.com>
2023-08-24 12:53:17 -0600

    feat(app): finished application

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -67,10 +67,10 @@ const AddPage = () => {
     const data = new FormData();
     data.append("file", file!);
     data.append("upload_preset", "restaurant");
+    console.log(data)
 
-    const res = await fetch("https://api.cloudinary.com/v1_1/dj3cp9q5g/image", {
+    const res = await fetch("https://api.cloudinary.com/v1_1/dj3cp9q5g/image/upload", {
       method: "POST",
-      headers: { "Content-Type": "multipart/form-data" },
       body: data,
     });
 
